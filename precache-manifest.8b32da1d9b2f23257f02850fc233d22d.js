2fc9864531c563307d875acc8fc407cd3293548c
Anna Pawl <annampawl@gmail.com>
2020-02-04 16:00:17 -0800

    Updates

---
+++ precache-manifest.8b32da1d9b2f23257f02850fc233d22d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f7f3065b5bd28f32a7fb91faf92d393",
+    "url": "/index.html"
+  },
+  {
+    "revision": "150a8b8ded7c22f49974",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a62a187f3aa8b2436a78",
+    "url": "/static/js/2.edebccea.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.edebccea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "150a8b8ded7c22f49974",
+    "url": "/static/js/main.d99238ae.chunk.js"
+  },
+  {
+    "revision": "dc4ffa467b3e2b94fe66",
+    "url": "/static/js/runtime-main.87dfc043.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
